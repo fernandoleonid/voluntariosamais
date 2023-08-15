97c21c4ce95584d22e285af2f375b0d429fd0c09
Fernando Leonid <fernandoleonid@gmail.com>
2023-08-15 00:39:23 -0300

    updade

--- voluntarios.js
+++ voluntarios.js
@@ -1,7 +1,7 @@
 // https://products.aspose.app/cells/pt/conversion/excel-to-json
 export const voluntarios = [
     {
-     "cleyde": "7\/19\/2023 10:41:55",
+     "Data Registo": "7\/19\/2023 10:41:55",
      "Endereço de e-mail": "maparecidaoliv@yahoo.com.br",
      "Digite seu nome": "Cida Oliveira",
      "Celular": 983057319,
@@ -13,7 +13,7 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "14h às 19h"
     },
     {
-     "cleyde": "7\/19\/2023 15:01:20",
+     "Data Registo": "7\/19\/2023 15:01:20",
      "Endereço de e-mail": "alecaroline1012@gmail.com",
      "Digite seu nome": "Alessandra de Moraes ",
      "Celular": "(11)99814-5506",
@@ -21,7 +21,7 @@ export const voluntarios = [
      "Domingo 13 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "7\/19\/2023 17:15:59",
+     "Data Registo": "7\/19\/2023 17:15:59",
      "Endereço de e-mail": "anamarchi2004@gmail.com",
      "Digite seu nome": "Ana Júlia Marchi da Cruz",
      "Celular": "(11)94370-9231",
@@ -33,14 +33,14 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "09h às 14h"
     },
     {
-     "cleyde": "7\/22\/2023 13:50:28",
+     "Data Registo": "7\/22\/2023 13:50:28",
      "Endereço de e-mail": "martinhomendescristiane@gmail.com",
      "Digite seu nome": "Cristiane Martinho Mendes ",
      "Celular": 1141522798,
      "Domingo 13 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "7\/22\/2023 14:24:46",
+     "Data Registo": "7\/22\/2023 14:24:46",
      "Endereço de e-mail": "marcos_soares.fonseca@hotmail.com",
      "Digite seu nome": "Marcos Soares da Fonseca ",
      "Celular": 11971631578,
@@ -48,14 +48,14 @@ export const voluntarios = [
      "Sábado 12 de Agosto": "18h às 23h"
     },
     {
-     "cleyde": "7\/22\/2023 14:25:46",
+     "Data Registo": "7\/22\/2023 14:25:46",
      "Endereço de e-mail": "danielle.sanches080@gmail.com",
      "Digite seu nome": "Danielle de Assis Sanches ",
      "Celular": "11 95917 4400 ",
      "Quinta-feira 10 de Agosto": "17h às 20h"
     },
     {
-     "cleyde": "7\/22\/2023 14:47:38",
+     "Data Registo": "7\/22\/2023 14:47:38",
      "Endereço de e-mail": "alesandrakfsl@hotmail.com",
      "Digite seu nome": "Alesandra Souza",
      "Celular": 11930986417,
@@ -70,7 +70,7 @@ export const voluntarios = [
      "Terça-feira 15 de Agosto": "18h às 23h"
     },
     {
-     "cleyde": "7\/23\/2023 19:35:19",
+     "Data Registo": "7\/23\/2023 19:35:19",
      "Endereço de e-mail": "lenacardoso47186484@gmail.com",
      "Digite seu nome": "Maria Madalena Cardoso de Mario",
      "Celular": 11963879555,
@@ -78,10 +78,11 @@ export const voluntarios = [
      "Terça-feira 08 de Agosto": "16h às 19h",
      "Quarta-feira 09 de Agosto": "20h às 23h",
      "Quinta-feira 10 de Agosto": "17h às 20h",
-     "Segunda-feira 14 de Agosto": "17h às 20h"
+     "Segunda-feira 14 de Agosto": "17h às 20h",
+     "Terça-feira 15 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "7\/24\/2023 8:46:24",
+     "Data Registo": "7\/24\/2023 8:46:24",
      "Endereço de e-mail": "thaynacca5@gmail.com",
      "Digite seu nome": "Thayná Cristina Cobello Alves ",
      "Celular": "11 996333797",
@@ -90,7 +91,7 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "09h às 14h"
     },
     {
-     "cleyde": "7\/24\/2023 9:39:37",
+     "Data Registo": "7\/24\/2023 9:39:37",
      "Endereço de e-mail": "milena.tozi@gmail.com",
      "Digite seu nome": "Milena Tozi",
      "Celular": 11950400699,
@@ -98,14 +99,14 @@ export const voluntarios = [
      "Quinta-feira 10 de Agosto": "20h às 23h"
     },
     {
-     "cleyde": "7\/24\/2023 13:35:21",
+     "Data Registo": "7\/24\/2023 13:35:21",
      "Endereço de e-mail": "bia.andrade1@hotmail.com",
      "Digite seu nome": "Beatriz Andrade ",
      "Celular": 11971211452,
      "Sábado 12 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "7\/24\/2023 20:35:27",
+     "Data Registo": "7\/24\/2023 20:35:27",
      "Endereço de e-mail": "prof.andersomborba@hotmail.com",
      "Digite seu nome": "- CAIXA - Andersom Luis de Borba",
      "Celular": "(11) 96397-2610",
@@ -113,7 +114,7 @@ export const voluntarios = [
      "Segunda-feira 14 de Agosto": "17h às 20h"
     },
     {
-     "cleyde": "7\/25\/2023 6:54:20",
+     "Data Registo": "7\/25\/2023 6:54:20",
      "Endereço de e-mail": "ijprandi@hotmail.com",
      "Digite seu nome": "- CAIXA - Iran José Prandi ",
      "Celular": "11 999785955 ",
@@ -121,7 +122,7 @@ export const voluntarios = [
      "Quinta-feira 10 de Agosto": "17h às 20h"
     },
     {
-     "cleyde": "7\/25\/2023 8:36:16",
+     "Data Registo": "7\/25\/2023 8:36:16",
      "Endereço de e-mail": "tatiana.franceschi@gmail.com",
      "Digite seu nome": "Tatiana Franceschi de Oliveira ",
      "Celular": 11996761010,
@@ -129,14 +130,14 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "09h às 14h"
     },
     {
-     "cleyde": "7\/26\/2023 11:14:11",
+     "Data Registo": "7\/26\/2023 11:14:11",
      "Endereço de e-mail": "jessica.delf@outlook.com",
      "Digite seu nome": "Jéssica Aparecida Delfino",
      "Celular": 11956695471,
      "Terça-feira 08 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "7\/27\/2023 9:16:16",
+     "Data Registo": "7\/27\/2023 9:16:16",
      "Endereço de e-mail": "gabrieltadeuxd@hotmail.com",
      "Digite seu nome": "GABRIEL TADEU DE ALMEIDA",
      "Celular": 11956102967,
@@ -144,14 +145,14 @@ export const voluntarios = [
      "Quinta-feira 10 de Agosto": "20h às 23h"
     },
     {
-     "cleyde": "7\/28\/2023 21:38:13",
+     "Data Registo": "7\/28\/2023 21:38:13",
      "Endereço de e-mail": "carolinaalvesdasilvia@gmail.com",
      "Digite seu nome": "Carolina Camargo Alves ",
      "Celular": 11961900448,
      "Terça-feira 08 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "7\/30\/2023 19:14:06",
+     "Data Registo": "7\/30\/2023 19:14:06",
      "Endereço de e-mail": "fatinha200467@yahoo.com.br",
      "Digite seu nome": "Maria de Fátima Paiva de Miranda ",
      "Celular": 11991596469,
@@ -159,17 +160,15 @@ export const voluntarios = [
      "Domingo 13 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "7\/30\/2023 19:14:20",
+     "Data Registo": "7\/30\/2023 19:14:20",
      "Endereço de e-mail": "nairanballesta@gmail.com",
      "Digite seu nome": "- CAIXA - Nairan Ballesta",
      "Celular": "(11) 98235-9926",
      "Segunda-feira 07 de Agosto": "19h às 23h",
-     "Sábado 12 de Agosto": "18h às 23h",
-     "Domingo 13 de Agosto": "13h às 18h",
-     "Terça-feira 15 de Agosto": "18h às 23h"
+     "Sábado 12 de Agosto": "18h às 23h"
     },
     {
-     "cleyde": "7\/30\/2023 20:29:39",
+     "Data Registo": "7\/30\/2023 20:29:39",
      "Endereço de e-mail": "anapaulasoares310@gmail.com",
      "Digite seu nome": "Ana Paula da Silva Soares ",
      "Celular": 11997519501,
@@ -178,7 +177,7 @@ export const voluntarios = [
      "Domingo 13 de Agosto": "18h às 23h"
     },
     {
-     "cleyde": "7\/30\/2023 20:35:59",
+     "Data Registo": "7\/30\/2023 20:35:59",
      "Endereço de e-mail": "daniela9394@outlook.com",
      "Digite seu nome": "Daniela",
      "Celular": 11956711339,
@@ -186,7 +185,7 @@ export const voluntarios = [
      "Segunda-feira 14 de Agosto": "17h às 20h"
     },
     {
-     "cleyde": "7\/30\/2023 21:01:03",
+     "Data Registo": "7\/30\/2023 21:01:03",
      "Endereço de e-mail": "marenmachado@yahoo.com.br",
      "Digite seu nome": "Maren Gehrts Pinheiro Machado ",
      "Celular": "11 934527843",
@@ -194,7 +193,7 @@ export const voluntarios = [
      "Domingo 13 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "7\/31\/2023 12:11:59",
+     "Data Registo": "7\/31\/2023 12:11:59",
      "Endereço de e-mail": "dri.fran.dcfc@gmail.com",
      "Digite seu nome": "Drielli Cristine Brum de Brito Oliveira",
      "Celular": 11963767005,
@@ -208,21 +207,21 @@ export const voluntarios = [
      "Terça-feira 15 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "8\/1\/2023 9:50:21",
+     "Data Registo": "8\/1\/2023 9:50:21",
      "Endereço de e-mail": "suelengodinho21@outlook.com",
      "Digite seu nome": "Suelen Regina Godinho do Carmo ",
      "Celular": 11955560595,
      "Segunda-feira 07 de Agosto": "16h às 19h"
     },
     {
-     "cleyde": "8\/1\/2023 13:34:36",
+     "Data Registo": "8\/1\/2023 13:34:36",
      "Endereço de e-mail": "deniseangelica22@yahoo.com.br",
      "Digite seu nome": "Denise",
      "Celular": 11991827433,
      "Terça-feira 15 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "8\/2\/2023 6:09:22",
+     "Data Registo": "8\/2\/2023 6:09:22",
      "Endereço de e-mail": "sandra.andreotti@gmail.com",
      "Digite seu nome": "- CAIXA - Sandra Andreotti ",
      "Celular": 11997525151,
@@ -234,53 +233,53 @@ export const voluntarios = [
      "Sexta-feira 11 de Agosto": "17h às 20h",
      "Domingo 13 de Agosto": "18h às 23h",
      "Segunda-feira 14 de Agosto": "20h às 23h",
-     "Terça-feira 15 de Agosto": "13h às 18h",
+     "Terça-feira 15 de Agosto": "18h às 23h",
      "Quarta-feira 16 de Agosto": "09h às 14h"
     },
     {
-     "cleyde": "8\/2\/2023 9:13:38",
+     "Data Registo": "8\/2\/2023 9:13:38",
      "Endereço de e-mail": "jeh.c.freitas@gmail.com",
      "Digite seu nome": "Jéssica Cristiane de Freitas",
      "Celular": 11969073736,
      "Sábado 05 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "8\/2\/2023 9:23:18",
+     "Data Registo": "8\/2\/2023 9:23:18",
      "Endereço de e-mail": "amandasmros@gmail.com",
      "Digite seu nome": "Amanda ",
      "Celular": 1194508185,
      "Terça-feira 15 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "8\/2\/2023 9:26:24",
+     "Data Registo": "8\/2\/2023 9:26:24",
      "Endereço de e-mail": "mirele23alves@hotmail.com",
      "Digite seu nome": "Mirele ",
      "Celular": 11941035077,
      "Sábado 05 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "8\/2\/2023 9:34:51",
+     "Data Registo": "8\/2\/2023 9:34:51",
      "Endereço de e-mail": "keelemc@gmail.com",
      "Digite seu nome": "keelem Crystyna ",
      "Celular": 11996550208,
      "Sexta-feira 11 de Agosto": "20h às 23h"
     },
     {
-     "cleyde": "8\/2\/2023 13:43:37",
+     "Data Registo": "8\/2\/2023 13:43:37",
      "Endereço de e-mail": "bi-bi9@hotmail.com",
      "Digite seu nome": "Bianca Araujo de Moura ",
      "Celular": 11930083135,
      "Sexta-feira 11 de Agosto": "20h às 23h"
     },
     {
-     "cleyde": "8\/2\/2023 19:09:50",
+     "Data Registo": "8\/2\/2023 19:09:50",
      "Endereço de e-mail": "llehllopes@gmail.com",
      "Digite seu nome": "Letícia Lopes ",
      "Celular": "11 971850214",
      "Quarta-feira 09 de Agosto": "20h às 23h"
     },
     {
-     "cleyde": "8\/2\/2023 21:23:32",
+     "Data Registo": "8\/2\/2023 21:23:32",
      "Endereço de e-mail": "contaoleonid@gmail.com",
      "Digite seu nome": "Guadalupe del Carmen",
      "Celular": 11999391203,
@@ -291,9 +290,9 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "8\/2\/2023 21:34:57",
+     "Data Registo": "8\/2\/2023 21:34:57",
      "Endereço de e-mail": "mcleydec36@gmail.com",
-     "Digite seu nome": "- CAIXA - Maria Cleyde Gomes da Silva",
+     "Digite seu nome": "Maria Cleyde Gomes da Silva",
      "Celular": 11998559022,
      "Sexta-feira 04 de Agosto": "19h às 23h",
      "Sábado 05 de Agosto": "13h às 18h",
@@ -302,10 +301,11 @@ export const voluntarios = [
      "Terça-feira 08 de Agosto": "16h às 19h",
      "Quarta-feira 09 de Agosto": "20h às 23h",
      "Sábado 12 de Agosto": "13h às 18h",
+     "Terça-feira 15 de Agosto": "13h às 18h",
      "Quarta-feira 16 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "8\/2\/2023 22:12:04",
+     "Data Registo": "8\/2\/2023 22:12:04",
      "Endereço de e-mail": "contatoleonid@gmail.com",
      "Digite seu nome": "Sandra Mironti",
      "Celular": "11 97362-1144",
@@ -315,7 +315,7 @@ export const voluntarios = [
      "Domingo 13 de Agosto": "18h às 23h"
     },
     {
-     "cleyde": "8\/2\/2023 22:15:38",
+     "Data Registo": "8\/2\/2023 22:15:38",
      "Endereço de e-mail": "contatoleonid@gmail.com",
      "Digite seu nome": "Magda (Silvia)",
      "Celular": 11971657461,
@@ -324,7 +324,7 @@ export const voluntarios = [
      "Quinta-feira 10 de Agosto": "20h às 23h"
     },
     {
-     "cleyde": "8\/3\/2023 13:45:24",
+     "Data Registo": "8\/3\/2023 13:45:24",
      "Endereço de e-mail": "bia44_7@msn.com",
      "Digite seu nome": "Maria Beatriz Carlassara Barral ",
      "Celular": "11 97467-1773",
@@ -338,14 +338,14 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "8\/3\/2023 21:25:01",
+     "Data Registo": "8\/3\/2023 21:25:01",
      "Endereço de e-mail": "hsmeyer1@gmail.com",
      "Digite seu nome": "Henrique Meyer",
      "Celular": 985860888,
      "Sexta-feira 11 de Agosto": "20h às 23h"
     },
     {
-     "cleyde": "8\/4\/2023 10:55:35",
+     "Data Registo": "8\/4\/2023 10:55:35",
      "Endereço de e-mail": "glaudineia2016@gmail.com",
      "Digite seu nome": "Neia (Claudia)",
      "Celular": "11 9 7279-0967 ",
@@ -362,7 +362,7 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "8\/4\/2023 11:56:35",
+     "Data Registo": "8\/4\/2023 11:56:35",
      "Endereço de e-mail": "contatoleonid@gmail.com",
      "Digite seu nome": "TINA",
      "Celular": "11 94089-2508",
@@ -370,7 +370,7 @@ export const voluntarios = [
      "Terça-feira 08 de Agosto": "16h às 19h"
     },
     {
-     "cleyde": "8\/4\/2023 14:52:05",
+     "Data Registo": "8\/4\/2023 14:52:05",
      "Endereço de e-mail": "dagmaroliveiramorgado@gmail.com",
      "Digite seu nome": "Dagmar de Oliveira Morgado ",
      "Celular": "11 957920818",
@@ -379,21 +379,21 @@ export const voluntarios = [
      "Segunda-feira 14 de Agosto": "17h às 20h"
     },
     {
-     "cleyde": "8\/6\/2023 12:03:45",
+     "Data Registo": "8\/6\/2023 12:03:45",
      "Endereço de e-mail": "srschumacker@hotmail.com.br",
      "Digite seu nome": "Silvia Schumacker ",
      "Celular": 11972657155,
      "Sábado 12 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "8\/6\/2023 12:57:30",
+     "Data Registo": "8\/6\/2023 12:57:30",
      "Endereço de e-mail": "wendel.bispo74@gmail.com",
      "Digite seu nome": "Marinalva Santos",
      "Celular": 11989309549,
      "Quarta-feira 16 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "8\/7\/2023 6:38:16",
+     "Data Registo": "8\/7\/2023 6:38:16",
      "Endereço de e-mail": "leeh.cristiny97@hotmail.com",
      "Digite seu nome": "Alessandra Cristine de Oliveira Nascimento ",
      "Celular": 11995866314,
@@ -401,14 +401,14 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "8\/7\/2023 11:53:58",
+     "Data Registo": "8\/7\/2023 11:53:58",
      "Endereço de e-mail": "srschumacker@hotmail.com.br",
      "Digite seu nome": "Silvia Schumacker ",
      "Celular": 11972657155,
      "Quarta-feira 09 de Agosto": "20h às 23h"
     },
     {
-     "cleyde": "8\/7\/2023 16:10:52",
+     "Data Registo": "8\/7\/2023 16:10:52",
      "Endereço de e-mail": "fernandoleonid@gmail.com",
      "Digite seu nome": "- CAIXA - Cintia Gomes Camelo",
      "Celular": 11971657461,
@@ -419,14 +419,14 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "19h às 23h"
     },
     {
-     "cleyde": "8\/8\/2023 14:30:24",
+     "Data Registo": "8\/8\/2023 14:30:24",
      "Endereço de e-mail": "silviacpasserini@gmail.com",
      "Digite seu nome": "Silvia Passerini ",
      "Celular": 11972285052,
      "Terça-feira 08 de Agosto": "16h às 19h"
     },
     {
-     "cleyde": "8\/8\/2023 16:51:28",
+     "Data Registo": "8\/8\/2023 16:51:28",
      "Endereço de e-mail": "pr.riicardo@gmail.com",
      "Digite seu nome": "Rick",
      "Celular": 15996616523,
@@ -436,14 +436,14 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "14h às 19h"
     },
     {
-     "cleyde": "8\/9\/2023 7:52:13",
+     "Data Registo": "8\/9\/2023 7:52:13",
      "Endereço de e-mail": "deise.f.soares81@gmail.com",
      "Digite seu nome": "Deise",
      "Celular": "11 97104-1418 ",
      "Quarta-feira 09 de Agosto": "17h às 20h"
     },
     {
-     "cleyde": "8\/9\/2023 18:56:02",
+     "Data Registo": "8\/9\/2023 18:56:02",
      "Endereço de e-mail": "magdasuaguiar@gmail.com",
      "Digite seu nome": "Magda Aguiar",
      "Celular": 11997388958,
@@ -454,24 +454,38 @@ export const voluntarios = [
      "Quarta-feira 16 de Agosto": "14h às 19h"
     },
     {
-     "cleyde": "8\/11\/2023 23:43:47",
+     "Data Registo": "8\/11\/2023 23:43:47",
      "Endereço de e-mail": "larinha.barboza13@gmail.com",
      "Digite seu nome": "Lara Careaga Barboza",
      "Celular": "‪11 95500‑6340‬",
      "Sábado 05 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "8\/12\/2023 8:44:02",
+     "Data Registo": "8\/12\/2023 8:44:02",
      "Endereço de e-mail": "contaoleonid@gmail.com",
      "Digite seu nome": "Claudio",
      "Celular": "11 99727-2361",
      "Sábado 12 de Agosto": "13h às 18h"
     },
     {
-     "cleyde": "8\/12\/2023 8:44:42",
-     "Endereço de e-mail": "contatoleonid@gmail.com",
-     "Digite seu nome": "Paloma",
-     "Celular": "11 99707-8430",
-     "Sábado 12 de Agosto": "13h às 18h"
+     "Data Registo": "8\/14\/2023 14:15:18",
+     "Endereço de e-mail": "bi-bi9@hotmail.com",
+     "Digite seu nome": "Bianca Araujo de Moura ",
+     "Celular": 11930083135,
+     "Terça-feira 15 de Agosto": "13h às 18h"
+    },
+    {
+     "Data Registo": "8\/14\/2023 17:30:17",
+     "Endereço de e-mail": "lugoes9703@gmail.com",
+     "Digite seu nome": "Luciana ",
+     "Celular": 11997558103,
+     "Terça-feira 15 de Agosto": "13h às 18h"
+    },
+    {
+     "Data Registo": "8\/15\/2023 0:12:34",
+     "Endereço de e-mail": "fernandoleonid@gmail.com",
+     "Digite seu nome": "- CAIXA - Joyce",
+     "Celular": 11971657461,
+     "Terça-feira 15 de Agosto": "13h às 18h"
     }
    ]
